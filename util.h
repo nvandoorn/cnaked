06039e34bab7c5c33d91f6912539a86f884efd42
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-03-01 11:40:25 -0800

    Nuke failed util

--- util.h
+++
@@ -1,6 +0,0 @@
-#ifndef CNAKE_UTIL_H
-#define CNAKE_UTIL_H
-
-int print(char *formatter);
-
-#endif
--- util.test.c
+++
@@ -1,8 +0,0 @@
-
-#include "test-lib.h"
-#include "util.h"
-
-
-#include <stdlib.h>
-#include <stdio.h>
-
